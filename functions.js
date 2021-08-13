edbbdd56af869584b45c005f71ef746812f48b5f
adam-belahouane <adam_belahouane@outlook.com>
2021-08-13 15:53:10 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -120,15 +120,27 @@ Write a function "cutString" to create a new string without the first and last c
 */
 writeHeader("EXERCISE 9")
 const cutString = function(str) {
-    let result = str.slice 
+    let result = str.slice(1,str.length -1)
+    return result
 }
+console.log(cutString("hello"))
 
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 writeHeader("EXERCISE 10")
-/* WRITE YOUR CODE HERE */
+function giveMeRandom(n) {
 
+    let arrWithNums = [];
+    
+    for(i = 0; i < n; i++) {
+        arrWithNums.push(Math.floor(Math.random() * 10)); 
+    }
+    
+    return arrWithNums;
+}
+
+console.log(giveMeRandom(8));
 /* WHEN YOU ARE FINISHED
 Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
 */
